97af7db5506805b4ed46f00871e0d4144a8ed420
BISMAGHANI <159704908+BISMAGHANI@users.noreply.github.com>
2024-04-29 22:19:23 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+// Importing Inquirer & Chalk Packages
+import inquirer from "inquirer";
+import chalk from "chalk";
+// Display a Colorfull Welcome Meesage
+console.log(chalk.italic.bold.blueBright("\n \t\t My Word Counter Track"));
+console.log("=".repeat(60));
+// Prompt the user to enter Sentence
+let answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter a Sentence:"
+    }
+]);
+// Trimming the Sentence & splitting it into words based on "Spaces"
+let words = answers.sentence.trim().split(" ");
+// Analysis of user input sentences
+console.log("=".repeat(60));
+console.log(chalk.bold("- Sentence: "));
+console.log(words);
+console.log(chalk.bold(`\n - Word Counter: ${chalk.italic.yellowBright(words.length)}`));
+console.log("=".repeat(60));
